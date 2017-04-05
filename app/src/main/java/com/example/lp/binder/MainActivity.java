654ed1e83d032b72d148f8c6e95ff55b86101b6e
Romain Tempo (Medsic) <romaint69@gmail.com>
2017-04-05 11:57:44 +0200

    [FEAT] Added home fragment to main activity

--- app/src/main/java/com/example/lp/binder/MainActivity.java
+++ app/src/main/java/com/example/lp/binder/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.lp.binder;
 
+import android.app.Fragment;
 import android.support.annotation.IdRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.lp.binder.fragment.HomeFragment;
 import com.roughike.bottombar.BottomBar;
 import com.roughike.bottombar.OnTabSelectListener;
 
@@ -18,15 +20,26 @@ public class MainActivity extends AppCompatActivity {
         bottomBar.setOnTabSelectListener(new OnTabSelectListener() {
             @Override
             public void onTabSelected(@IdRes int tabId) {
-                if (tabId == R.id.tab_home) {
-                    System.out.println("TAB HOME");
+                Fragment fragment = null;
+                switch(tabId) {
+                    case R.id.tab_home:
+                        fragment = new HomeFragment();
+                        break;
+                    case R.id.tab_message:
 
+                        break;
+                    case R.id.tab_setting:
+
+                        break;
+                    case R.id.tab_account:
+
+                        break;
                 }
-
-                if (tabId == R.id.tab_message) {
-                    System.out.println("TAB MESSAGE");
+                if(fragment != null) {
+                    getFragmentManager().beginTransaction().add(R.id.contentContainer, fragment).commit();
                 }
             }
         });
+        getFragmentManager().beginTransaction().add(R.id.contentContainer, new HomeFragment()).commit();
     }
 }
