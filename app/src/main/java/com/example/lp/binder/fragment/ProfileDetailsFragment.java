99ef7feef407a1ca9662b0e7d7eeeb305d769e8f
Marimax <maxime.ducoroy01@gmail.com>
2017-05-11 17:14:47 +0200

    ProfilFragment

--- app/src/main/java/com/example/lp/binder/fragment/ProfileDetailsFragment.java
+++ app/src/main/java/com/example/lp/binder/fragment/ProfileDetailsFragment.java
@@ -1,7 +1,9 @@
 package com.example.lp.binder.fragment;
 
 import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -10,8 +12,16 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.example.data.User;
+import com.example.lp.binder.ProfilCustom;
 import com.example.lp.binder.R;
 import com.example.lp.binder.adapter.PicturesGridAdapter;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -58,5 +68,61 @@ public class ProfileDetailsFragment extends Fragment {
         picturesGridView.setAdapter(adapter);
 
         return view;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        DatabaseReference refUser = FirebaseDatabase.getInstance().getReference().child("users").child(FirebaseAuth.getInstance().getCurrentUser().getUid()).getRef();
+        refUser.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if(dataSnapshot.child(User.NICKNAME).exists()){
+                    nameTextView.setText(dataSnapshot.child(User.NICKNAME).getValue(String.class));
+                }
+                if(dataSnapshot.child(User.AGE).exists()){
+                    ageTextView.setText(dataSnapshot.child(User.AGE).getValue(String.class) + "y.o");
+                }
+                String genderString = "";
+                if(dataSnapshot.child(User.GENDER).exists()){
+                    int gender = dataSnapshot.child(User.GENDER).getValue(Integer.class);
+                    genderString = getGender(gender);
+                }
+                if(dataSnapshot.child(User.GENDER_PREF).child(User.GENDER_PREF1).exists()){
+                    int gender = dataSnapshot.child(User.GENDER_PREF).child(User.GENDER_PREF1).getValue(Integer.class);
+                    genderString += "looking for a " + getGender(gender);
+                }
+                if(dataSnapshot.child(User.GENDER_PREF).child(User.GENDER_PREF2).exists()){
+                    int gender = dataSnapshot.child(User.GENDER_PREF).child(User.GENDER_PREF2).getValue(Integer.class);
+                    genderString += " and a " + getGender(gender);
+                }
+                lookingForTextView.setText(genderString);
+                if(dataSnapshot.child(User.DESCRIPTION).exists()){
+                    descriptionTextView.setText(dataSnapshot.child(User.DESCRIPTION).getValue(String.class));
+                }
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+        editButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getActivity(), ProfilCustom.class);
+                startActivity(i);
+            }
+        });
+
+
+    }
+
+    private String getGender(int gender){
+        if(gender == 0)
+            return "man";
+        else
+            return  "woman";
     }
 }
